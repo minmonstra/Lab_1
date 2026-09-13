5fc9d62c92db705caeee1c42f37776e3ca0e75ce
Margo <margoniya@outlook.com>
2026-09-13 12:19:57 +0700

    метод добавления нового протиника по заданным параметрам

--- Lab-1/CEnemyTemplateList.cs
+++ Lab-1/CEnemyTemplateList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Lab_1
 {
@@ -12,7 +13,15 @@ namespace Lab_1
         List<CEnemyTemplate> enemies;
         public CEnemyTemplateList()
         {
-            enemies = new List<CEnemyTemplate>();
+            enemies = new List<CEnemyTemplate>(); }
+        
+            public void AddEnemy(string name, string iconName, int baseLife, double lifeModifier, int baseGold, double goldModifier, double spawnChance)
+        {
+            CEnemyTemplate enemy = new CEnemyTemplate(name, iconName, baseLife, lifeModifier, baseGold, goldModifier, spawnChance);
+            enemies.Add(enemy);
         }
+        
     }
 }
+    
+
