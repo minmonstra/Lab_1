80519493b77a888970cf2293a122a99338f01bdf
Margo <margoniya@outlook.com>
2026-09-13 11:45:59 +0700

    добавлен класс для работы с  противниками

---
+++ Lab-1/CEnemyTemplateList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class CEnemyTemplateList
+    {
+        //Список противников из класса CEnemyTemplate
+        List<CEnemyTemplate> enemies;
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+    }
+}
