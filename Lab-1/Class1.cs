5bbce37d4e9a05bfb9fca364c982f1a77b766162
Margo <margoniya@outlook.com>
2026-09-13 11:41:48 +0700

    создали класс противника

---
+++ Lab-1/Class1.cs
@@ -0,0 +1,26 @@
+﻿public class CEnemyTemplate
+{
+    //Свойства класса — доступны для чтения снаружи,
+    //изменяются только изнутри класса (через конструктор)
+    public string Name { get; private set; }
+    public string IconName { get; private set; }
+    public int BaseLife { get; private set; }
+    public double LifeModifier { get; private set; }
+    public int BaseGold { get; private set; }
+    public double GoldModifier { get; private set; }
+    public double SpawnChance { get; private set; }
+    
+    //Конструктор класса
+    public CEnemyTemplate(string name, string iconName, int baseLife,
+    double lifeModifier, int baseGold,
+    double goldModifier, double spawnChance)
+    {
+        Name = name;
+        IconName = iconName;
+        BaseLife = baseLife;
+        LifeModifier = lifeModifier;
+        BaseGold = baseGold;
+        GoldModifier = goldModifier;
+        SpawnChance = spawnChance;
+    }
+}
